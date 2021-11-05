9e176700c89a70b47252a47deac77957e810ff92
Marcos Ceola <Marcos.VC321@gmail.com>
2021-11-04 21:04:05 -0300

    Criando repository e service para categoria

--- src/main/java/com/marcos/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/marcos/cursomc/resources/CategoriaResource.java
@@ -1,31 +1,32 @@
 package com.marcos.cursomc.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.marcos.cursomc.domain.Categoria;
+import com.marcos.cursomc.services.CategoriaService;
 
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
+	@Autowired
+	private CategoriaService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+
+		Categoria obj = service.buscar(id);
 		
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
-		
-		List<Categoria> lista = new ArrayList<>();
-		
-		lista.add(cat1);
-		lista.add(cat2);
-				
-		return lista;
+		/*retorna o RespnseEntity com ok com a operação ocorrendo com sucesso, e o corpo com o objeto que buscou*/
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
