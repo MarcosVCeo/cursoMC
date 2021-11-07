29857feb6775ece2eb6d0edb888a1216becc1178
Marcos Ceola <Marcos.VC321@gmail.com>
2021-11-07 11:34:07 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/marcos/cursomc/domain/Produto.java
+++ src/main/java/com/marcos/cursomc/domain/Produto.java
@@ -16,7 +16,6 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
@@ -29,7 +28,7 @@ public class Produto implements Serializable {
 	private String nome;
 	private Double preco;
 	
-	@JsonBackReference
+	@JsonIgnore
 	@ManyToMany
 	@JoinTable(name = "PRODUTO_CATEGORIA",
 		joinColumns = @JoinColumn(name = "produto_id"),
@@ -51,7 +50,7 @@ public class Produto implements Serializable {
 		this.preco = preco;
 	}
 	
-	@JsonBackReference
+	@JsonIgnore
 	public List<Pedido> getPedidos(){
 		List<Pedido> lista = new ArrayList<>();
 		for(ItemPedido x: itens) {
